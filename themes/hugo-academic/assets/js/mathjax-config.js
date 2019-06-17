0ac1fd2f4c9ac72a3ec1e0017a13bfbe1695ad7a
Alison Hill <apreshill@gmail.com>
2019-06-16 21:48:08 -0700

    clean slate

--- themes/hugo-academic/assets/js/mathjax-config.js
+++
@@ -1,6 +0,0 @@
-window.MathJax = {
-  CommonHTML: { linebreaks: { automatic: true } },
-  tex2jax: { inlineMath: [ ['$', '$'], ['\\(','\\)'] ], displayMath: [ ['$$','$$'], ['\\[', '\\]'] ], processEscapes: false },
-  TeX: { noUndefined: { attributes: { mathcolor: 'red', mathbackground: '#FFEEEE', mathsize: '90%' } } },
-  messageStyle: 'none'
-};
